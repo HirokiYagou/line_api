d1ebae5e12eda87a7bad8a9633d8759c6ebdf768
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-03-11 13:39:21 +0900

    Revert "Revert "serverフォルダ削除""
    This reverts commit 75d12501f3324e856d4d04ef7c89a15233cb91d5.

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
-
-// まずはルーティングのみ追加
-router.post('/webhook', require('./routes/webhook'))
-
-module.exports = router
